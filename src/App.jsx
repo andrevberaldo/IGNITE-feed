dd2dec1d4805aeb4423579e98b247cd5c3e25e68
andrevberaldo <andreberaldo@hotmail.com>
2022-11-12 10:49:56 -0300

    Sibar and Main content aligned

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 import Chance from "./Utils/Chance";
+
+import styles from './App.module.css';
 import './global.css';
 
 export function App() {
@@ -8,18 +10,28 @@ export function App() {
   return (
     <>
     <Header />
-      <Post 
-        author={Chance.fullName()}
-        content={Chance.content(2)}
-      />
-      <Post 
-        author={Chance.fullName()}
-        content={Chance.content(3)}
-      />
-      <Post 
-        author={Chance.name()}
-        content={Chance.content()}
-      />
+      
+    <div className={styles.wrapper}>
+      <aside>
+        sidebar
+      </aside>
+
+      <main>
+          <Post 
+            author={Chance.fullName()}
+            content={Chance.content(2)}
+          />
+          <Post 
+            author={Chance.fullName()}
+            content={Chance.content(3)}
+          />
+          <Post 
+            author={Chance.name()}
+          content={Chance.content()}
+          />
+        </main>
+    
+    </div>
     </>
   )
 }
