6a6b129dee0f4cd2c290739b4d468e68770c57d3
andrevberaldo <andreberaldo@hotmail.com>
2022-11-13 19:51:20 -0300

    sidebar finished, phospor dependency added

--- src/components/side-bar/SideBar.jsx
+++ src/components/side-bar/SideBar.jsx
@@ -1,17 +1,25 @@
 import rand from '../../Utils/Chance';
 import styles from './SideBar.module.css';
+import { PencilLine } from 'phosphor-react';
 
 export function SideBar() {
     return (
         <aside className={styles.sidebar}>
             <img className={styles.cover} src={rand.background()} alt="" />
             <div className={styles.profile}>
+                <img className={styles.avatar} src={rand.avatar()} alt="" />
+
                 <strong>{rand.fullName()}</strong>
                 <span>{rand.job()}</span>                
             </div>
 
-            <footer className={styles.footer}>
-                <a href="#">Edit Perfil</a>
+            <footer className={styles.footer}>                
+                <a href="#">
+                <PencilLine 
+                    size={20}
+                />
+                    Edit Perfil
+                </a>
             </footer>
         </aside>
     )
